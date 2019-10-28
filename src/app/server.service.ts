d7737731e68490903f88b4073cd11b4b93d97779
Alyse Bos <alyse.bos@gmail.com>
2019-10-28 12:06:48 -0700

    fixed

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://fathomless-hamlet-56105.herokuapp.com";
 
 @Injectable({
   providedIn: 'root'
