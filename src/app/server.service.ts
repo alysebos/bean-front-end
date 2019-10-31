38a61e3d27132f1059a3c2cef1b1e3c330d9e970
Alyse Bos <alyse.bos@gmail.com>
2019-10-30 20:00:02 -0700

    i swear typos will be the reason i die

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-const baseUrl = "https://fathkomless-hamlet-56105.herokuapp.com";
+const baseUrl = "https://fathomless-hamlet-56105.herokuapp.com";
 // const baseUrl = "http://localhost:8080";
 
 @Injectable({
